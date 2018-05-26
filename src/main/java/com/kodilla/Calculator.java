d6700ad5e4fe0db06f9de44a054c7b476f9bb162
Marcin Domański <marcindomanski@Mac-mini-Marcin.local>
2018-05-26 19:23:36 +0200

    Klasa dodająca oraz odejmująca liczby

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,30 @@
+package com.kodilla;
+
+public class Calculator {
+
+
+    public int add (int firstNumber, int secondNumber) {
+
+        return firstNumber + secondNumber;
+
+    }
+
+    public int subtract (int firstNumber, int secondNumber) {
+
+        return firstNumber - secondNumber;
+
+    }
+
+}
+
+class AppCalculator {
+
+    public static void main (String args[]) {
+
+        Calculator appCalculator = new Calculator();
+        int addResult = appCalculator.add(13, 21);
+        int subtractResult = appCalculator.subtract(9, 12);
+
+        System.out.println(subtractResult);
+    }
+}
